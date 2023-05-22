c5ea6b8d35c49fed7a7d4d681aec3653d5333f65
UmarRamu <jahidfathur@gmail.com>
2023-05-22 17:13:25 +0700

    update

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -39,7 +39,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
           />
         </div>
         <p className="font-normal text-[16px] leading-[20px] text-white uppercase">
-          Enter the metaverse
+          Enter the metaversis
         </p>
         <h2 className="mt-[24px] font-semibold sm:text-[32px] text[24px] text-white">
           {title}
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-          Enter the Metaverse
+          Enter the Metaversis
         </h4>
         <button
           type="button"
@@ -30,7 +30,7 @@ const Footer = () => (
             className="w-[24px] h-[24px] object-contain"
           />
           <span className="font-normal text-[16px] text-white">
-            ENTER THE METAVERSE
+            ENTER THE METAVERSIS
           </span>
         </button>
       </div>
@@ -39,7 +39,7 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">Metaversis</h4>
+          <h4 className="font-extrabold text-[24px] text-white">Ramuow</h4>
           <p className="font-normal text-[14px] text-white opacity-50">
             Copyright © 2023 Metaversis. All rights reserved.
           </p>
